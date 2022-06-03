78ed38968b07b022bda8de10aa00563ca822c9c4
Zhakyp <73534500+Zhakyp01@users.noreply.github.com>
2022-06-03 10:11:34 +0600

    Add files via upload

---
+++ Ultrasonic_Servo.ino
@@ -0,0 +1,48 @@
+#include <Servo.h>
+
+Servo servo;
+
+int trigPin = 9;
+int echoPin = 8;
+
+
+long duration;
+int distance;
+
+void setup() 
+{
+  servo.attach(7);
+  servo.write(0);
+ delay(2000);
+  
+
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+Serial.begin(9600); 
+}
+void loop() 
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+duration = pulseIn(echoPin, HIGH);
+distance= duration*0.034/2;
+
+Serial.print("Distance: ");
+Serial.println(distance);
+if ( distance <= 40   ) 
+ {
+
+servo.write(0);
+delay(3500);
+ } 
+else 
+{
+servo.write(90);
+
+ }
+
+}
